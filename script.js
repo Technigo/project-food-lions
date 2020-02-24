9e87bad1b7e35b86ef3297cc3b6e9f2b61b61f81
Emelie Loheden <emelie_loheden@hotmail.com>
2020-02-24 14:22:10 +0100

    finished step 2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const restaurants = document.getElementById("restaurants")
+
 fetch('https://developers.zomato.com/api/v2.1/search?entity_id=91&entity_type=city&cuisines=82',
     { headers: { 'user-key': "8e3b45cc8d20404f3751e42d9cef2e88" } }
 )
@@ -6,6 +8,17 @@ fetch('https://developers.zomato.com/api/v2.1/search?entity_id=91&entity_type=ci
     })
     .then(info => {
         console.log(info)
+
+        info.restaurants.forEach(item => {
+         restaurants.innerHTML += `<p>Restaurant name: ${item.restaurant.name} 
+         Average cost: ${item.restaurant.currency} ${item.restaurant.average_cost_for_two/2}
+         Address ${item.restaurant.location.address}
+         </p> <img src="${item.restaurant.thumb}">
+         <p>Review: ${item.restaurant.user_rating.aggregate_rating} /5 ${item.restaurant.user_rating.rating_text}</p>
+         `   
+        });
+
+
 
 
     })
