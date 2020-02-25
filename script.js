3771dd968b267daaaf00b70c82aa51fff17e3904
Sickan-sys <57603670+Sickan-sys@users.noreply.github.com>
2020-02-25 13:35:57 +0100

    reviews

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-const restaurants = document.getElementById("restaurants")
-
-fetch('https://developers.zomato.com/api/v2.1/search?entity_id=91&entity_type=city&cuisines=82',
-    { headers: { 'user-key': "8e3b45cc8d20404f3751e42d9cef2e88" } }
+const restaurants = document.getElementById("restaurants");
+const reviewList = document.getElementById("reviewList");
+fetch(
+  "https://developers.zomato.com/api/v2.1/search?entity_id=91&entity_type=city&cuisines=82",
+  { headers: { "user-key": "8e3b45cc8d20404f3751e42d9cef2e88" } }
 )
-    .then(response => {
-        return response.json()
-    })
-    .then(info => {
-        console.log(info)
-
-        info.restaurants.forEach(item => {
-         restaurants.innerHTML += `<p>Restaurant name: ${item.restaurant.name} 
-         Average cost: ${item.restaurant.currency} ${item.restaurant.average_cost_for_two/2}
+  .then(response => {
+    return response.json();
+  })
+  .then(info => {
+    console.log(info);
+    info.restaurants.forEach(item => {
+      restaurants.innerHTML += `<p>Restaurant name: ${item.restaurant.name} 
+         Average cost: ${item.restaurant.currency} ${item.restaurant
+        .average_cost_for_two / 2}
          Address ${item.restaurant.location.address}
          </p> <img src="${item.restaurant.thumb}">
-         <p>Review: ${item.restaurant.user_rating.aggregate_rating} /5 ${item.restaurant.user_rating.rating_text}</p>
-         `   
-        });
-
-
-
-
+         <p>Review: ${item.restaurant.user_rating.aggregate_rating} /5 ${
+        item.restaurant.user_rating.rating_text
+      }</p>
+        <div id="review-${item.restaurant.id}"></div>`;
+      showReviews(item.restaurant.id);
+    });
+  });
+const showReviews = id => {
+  fetch(`https://developers.zomato.com/api/v2.1/reviews?res_id=${id}`, {
+    headers: { "user-key": "8e3b45cc8d20404f3751e42d9cef2e88" }
+  })
+    .then(response => {
+      return response.json();
     })
+    .then(review => {
+      const slice = review.user_reviews.slice(0,3)
+      slice.forEach(item => {
+        return (document.getElementById(`review-${id}`).innerHTML += `<p> ${
+          item.review.review_text
+        } </p>`);
+      });
+    });
+};
