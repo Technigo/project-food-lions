838bd83651cc11c6903a2d0b07b34e36f2db60f1
linneanylen <48217204+linneanylen@users.noreply.github.com>
2020-02-25 09:57:22 +0100

    changed to radiobuttons

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 const restaurants = document.getElementById("restaurants")
-const highestFirst = (info) => {
+const sortPrice = (info) => {
 
-
-    info.sort(function (a, b) {
-        return b.restaurant.average_cost_for_two - a.restaurant.average_cost_for_two
-
-    })
+    if (document.getElementById('highestFirst').checked) {
+        info.sort(function (a, b) {
+            return b.restaurant.average_cost_for_two - a.restaurant.average_cost_for_two
+        })
+    }
+    else if (document.getElementById('lowestFirst').checked) {
+        info.sort(function (a, b) {
+            return a.restaurant.average_cost_for_two - b.restaurant.average_cost_for_two
+        })
+    }
 
     console.log(info)
     restaurants.innerHTML = ""
 
-
     info.forEach(item => {
         restaurants.innerHTML += `<p>Restaurant name: ${item.restaurant.name} 
-     Average cost: ${item.restaurant.currency} ${item.restaurant.average_cost_for_two / 2}
-     Address ${item.restaurant.location.address}
-     </p> <img src="${item.restaurant.thumb}">
-     <p>Review: ${item.restaurant.user_rating.aggregate_rating} /5 ${item.restaurant.user_rating.rating_text}</p>
-     `
+ Average cost: ${item.restaurant.currency} ${item.restaurant.average_cost_for_two / 2}
+ Address ${item.restaurant.location.address}
+ </p> <img src="${item.restaurant.thumb}">
+ <p>Review: ${item.restaurant.user_rating.aggregate_rating} /5 ${item.restaurant.user_rating.rating_text}</p>
+ `
     })
 
 };
+
+
 
 fetch('https://developers.zomato.com/api/v2.1/search?entity_id=91&entity_type=city&cuisines=82',
     { headers: { 'user-key': "8e3b45cc8d20404f3751e42d9cef2e88" } }
@@ -30,12 +36,15 @@ fetch('https://developers.zomato.com/api/v2.1/search?entity_id=91&entity_type=ci
     })
     .then(info => {
 
-
+        info.restaurants.sort(function (a, b) {
+            return a.restaurant.average_cost_for_two - b.restaurant.average_cost_for_two
+        })
 
         console.log(info)
 
 
-        document.getElementById('test').addEventListener("click", () => highestFirst(info.restaurants))
+        document.getElementById('highestFirst').addEventListener("change", () => sortPrice(info.restaurants))
+        document.getElementById('lowestFirst').addEventListener("change", () => sortPrice(info.restaurants))
 
         info.restaurants.forEach(item => {
             restaurants.innerHTML += `<p>Restaurant name: ${item.restaurant.name} 
