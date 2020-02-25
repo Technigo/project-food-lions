51a201a6a43b9186f1efb608314bcd5e37e64b7e
Johanna Åstrand <johanna@Johannas-MacBook-Pro.local>
2020-02-25 12:35:43 +0100

    added filter function for delivery, takeaway and on price range

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const restaurants = document.getElementById("restaurants")
-
+let restoInfo;
+let averageCost;
 fetch('https://developers.zomato.com/api/v2.1/search?entity_id=91&entity_type=city&cuisines=82',
     { headers: { 'user-key': "8e3b45cc8d20404f3751e42d9cef2e88" } }
 )
@@ -8,24 +8,48 @@ fetch('https://developers.zomato.com/api/v2.1/search?entity_id=91&entity_type=ci
     })
     .then(info => {
         console.log(info)
-
-        info.restaurants.forEach(item => {
-            restaurants.innerHTML += `<p>Restaurant name: ${item.restaurant.name} 
-         Average cost: ${item.restaurant.currency} ${item.restaurant.average_cost_for_two / 2}
-         Address ${item.restaurant.location.address}
-         </p> <img src="${item.restaurant.thumb}">
-         <p>Review: ${item.restaurant.user_rating.aggregate_rating} /5 ${item.restaurant.user_rating.rating_text}</p>
-         `
-
-            //const startsWithB = pokemons.filter(pokemons => pokemons.startsWith("B"));
-            const filterFunction = () => {
-                const deliveryOption = item.restaurant.highlights
-                deliveryOption.filter(deliveryOption => deliveryOption.includes("Delivery"))
-                console.log(deliveryOption)
-            }
-
-
-            filterFunction()
-            document.getElementById('hasDelivery').addEventListener('click', filterFunction)
-        })
-    });
+        restoInfo = info.restaurants
+        showRestaurant(info.restaurants);
+    })
+const showRestaurant = (array) => {
+    console.log("show restau", array.length, array)
+    restaurants.innerHTML = `<p></p>`
+    array.forEach(item => {
+        restaurants.innerHTML += `<p>Restaurant name: ${item.restaurant.name} 
+ Average cost: ${item.restaurant.currency} ${item.restaurant.average_cost_for_two / 2}
+ Address ${item.restaurant.location.address}
+ </p> <img src="${item.restaurant.thumb}">
+ <p>Review: ${item.restaurant.user_rating.aggregate_rating} /5 ${item.restaurant.user_rating.rating_text}</p>
+ `
+    })
+}
+const filterFunction = () => {
+    const filteredArray = restoInfo.filter(item => item.restaurant.highlights.includes("Takeaway Available"))
+    console.log(filteredArray);
+    showRestaurant(filteredArray);
+};
+const filterDeliveryFunction = () => {
+    const filteredArray = restoInfo.filter(item => item.restaurant.highlights.includes("Delivery"))
+    console.log(filteredArray);
+    showRestaurant(filteredArray);
+};
+const filterPriceLow = () => {
+    const filteredArray = restoInfo.filter(item => item.restaurant.average_cost_for_two <= 30)
+    console.log(filteredArray);
+    showRestaurant(filteredArray);
+};
+const filterPriceMedium = () => {
+    const filteredArray = restoInfo.filter(item => item.restaurant.average_cost_for_two > 30 && item.restaurant.average_cost_for_two < 45)
+    console.log(filteredArray);
+    showRestaurant(filteredArray);
+};
+const filterPriceHigh = () => {
+    const filteredArray = restoInfo.filter(item => item.restaurant.average_cost_for_two >= 45)
+    console.log(filteredArray);
+    showRestaurant(filteredArray);
+};
+document.getElementById('hasTakeaway').addEventListener('click', filterFunction)
+document.getElementById('hasDelivery').addEventListener('click', filterDeliveryFunction)
+document.getElementById('priceLow').addEventListener('click', filterPriceLow)
+document.getElementById('priceMedium').addEventListener('click', filterPriceMedium)
+document.getElementById('priceHigh').addEventListener('click', filterPriceHigh)
