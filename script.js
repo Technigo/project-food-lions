5fdd63de9202451cad99c075448b753b2aeb54c9
Johanna Åstrand <johanna@Johannas-MacBook-Pro.local>
2020-02-25 09:14:44 +0100

    first test

--- script.js
+++ script.js
@@ -10,15 +10,22 @@ fetch('https://developers.zomato.com/api/v2.1/search?entity_id=91&entity_type=ci
         console.log(info)
 
         info.restaurants.forEach(item => {
-         restaurants.innerHTML += `<p>Restaurant name: ${item.restaurant.name} 
-         Average cost: ${item.restaurant.currency} ${item.restaurant.average_cost_for_two/2}
+            restaurants.innerHTML += `<p>Restaurant name: ${item.restaurant.name} 
+         Average cost: ${item.restaurant.currency} ${item.restaurant.average_cost_for_two / 2}
          Address ${item.restaurant.location.address}
          </p> <img src="${item.restaurant.thumb}">
          <p>Review: ${item.restaurant.user_rating.aggregate_rating} /5 ${item.restaurant.user_rating.rating_text}</p>
-         `   
-        });
+         `
+
+            //const startsWithB = pokemons.filter(pokemons => pokemons.startsWith("B"));
+            const filterFunction = () => {
+                const deliveryOption = item.restaurant.highlights
+                deliveryOption.filter(deliveryOption => deliveryOption.includes("Delivery"))
+                console.log(deliveryOption)
+            }
 
 
-
-
-    })
+            filterFunction()
+            document.getElementById('hasDelivery').addEventListener('click', filterFunction)
+        })
+    });
